5414561ffc46e31dc910766041bcbb7a20733b0c
junior <juniormathiias@gmail.com>
2024-03-07 21:37:57 -0300

    added the start_with and end_with matchers also regex match

--- spec/matchers/comparacao/comparacao_spec.rb
+++ spec/matchers/comparacao/comparacao_spec.rb
@@ -14,5 +14,22 @@ describe 'Matchers de comparação' do
   it 'Compare <=' do 
     expect(5).to be >= 5
   end
+
+  it 'be_between inclusive' do 
+    expect(5).to be_between(2,7).inclusive
+  end
+
+  it 'be_between exclusive' do 
+    expect(5).to be_between(2,7).exclusive
+  end
+
+  it 'match regex' do 
+    expect("fulando@.com.br").to match(/..@../)
+  end
+
+  it 'start with and end with' do 
+    expect("fulando@.com.br").to start_with("fu")
+    expect("fulando@.com.br").to end_with("br")
+  end
   
   end
