330c33b94a0f20f46ac3a035427af3feafac4187
junior <juniormathiias@gmail.com>
2024-03-11 22:18:42 -0300

    added the matchers error

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,9 @@ class Calculator
   def sum(a, b)
     a + b
   end
+
+  def div(a,b)
+    a / b
+  end
+
 end
--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -2,6 +2,17 @@ require 'calculator'
 
 describe Calculator do
 
+  context 'diviside ' do
+    it 'by 0' do
+      expect{subject.div(3,0)}.to raise_exception
+    end
+    it 'by 0' do
+      expect{subject.div(3,0)}.to raise_error(ZeroDivisionError)
+      expect{subject.div(3,0)}.to raise_error("divided by 0")
+      
+    end
+  end
+
   context '#sum' do
     it 'with positive numbers' do
       result = subject.sum(5, 7)
