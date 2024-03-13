4051771f0c1c8e8b3123b790a02ea2e10d7b6087
junior <juniormathiias@gmail.com>
2024-03-12 22:16:27 -0300

    added the One-liner syntax

---
+++ spec/matchers/ranges/ranges_spec.rb
@@ -0,0 +1,10 @@
+describe (1..5), 'Rangers' do
+  it '#cover' do
+    expect(subject).to cover(2)
+    is_expected.to cover(3)
+    expect(subject).not_to cover(6)
+  end
+
+  it { is_expected.to cover(3)}
+  it { is_expected.not_to cover(3)}
+end
