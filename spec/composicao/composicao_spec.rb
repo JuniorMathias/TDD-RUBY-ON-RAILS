f6a2dee217e6e395d547875a508789b04032835b
junior <juniormathiias@gmail.com>
2024-03-15 16:30:25 -0300

    movendo funcao para um helper

--- spec/composicao/composicao_spec.rb
+++ spec/composicao/composicao_spec.rb
@@ -5,6 +5,7 @@ describe 'Ruby on Rails' do
   it {is_expected.to start_with("Ruby").and end_with("Rails")}
   it {expect(fruta).to eq('banana').or eq('uva').or eq('laranja')}
 
+  #essa função foi movida para um helper 
   # def fruta
   #   %w(banana laranja uva).sample
   # end
