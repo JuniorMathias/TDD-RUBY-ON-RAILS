5e2c4029a49b16b0f0a7d3732e1b9e4e7bb616c8
junior <juniormathiias@gmail.com>
2024-03-20 16:00:05 -0300

    tag filters

--- spec/matchers/arrays/arrays_spec.rb
+++ spec/matchers/arrays/arrays_spec.rb
@@ -1,12 +1,13 @@
 #negativando o matcher
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe Array.new([1,2,3]) do
+describe Array.new([1,2,3]), "Array", collection: true do
+  #pode se usar somente :collection também uo type: 'collection'
   it '#include' do
     expect(subject).to include(2)
     expect(subject).to exclude(4)
   end
-  it '#matcher' do
+  it '#matcher', :slow do
     expect(subject).to match_array([1,2,3])
   end
   it '#contain_exactly' do
